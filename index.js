c0a1d16e3cf067a3b1a871c6c81057d4884dd1e9
youngchul.kim <youngck.uw@gmail.com>
2018-02-19 22:45:02 -0800

    mobile friendly from 320px and above, removal of tabindex=0 and other unused tags, adding legend

--- index.js
+++ index.js
@@ -5,17 +5,28 @@ let questionNumber = 0;
 
 function displayQuiz(){
 	$('main').html(
-		`<p>Current Score ${score + "/" + questions.length}</p>
-		 <h2>${questions[questionNumber].question}</h2> 
+		`<div id='score'><p>Score ${score} | Question ${questionNumber + "/" + questions.length}</p></div>
+		 <div id='questionText'><h2>${questions[questionNumber].question}</h2></div> 
 		 <form id='questionForm' action="/some-server-endpoint" method ='post'> 
-		 <fieldset name='answerChoices'> 
-		 <div><label><input type='radio' name='answer' id='a' required><span>\t${questions[questionNumber].answer.a}</span></label></div> 
-		 <div><label><input type='radio' name='answer' id='b' required><span>\t${questions[questionNumber].answer.b}</span></label></div>
-		 <div><label><input type='radio' name='answer' id='c' required><span>\t${questions[questionNumber].answer.c}</span></label></div> 
-		 <div><label><input type='radio' name='answer' id='d' required><span>\t${questions[questionNumber].answer.d}</span></label></div> 
-		 </fieldset> 
+		 <fieldset name='answerChoices'><legend><h2>Answers:</h2></legend>
+		 <label><input type='radio' name='answer' id='a' required><span>\t${questions[questionNumber].answer.a}</span></label> 
+		 <label><input type='radio' name='answer' id='b' required><span>\t${questions[questionNumber].answer.b}</span></label>
+		 <label><input type='radio' name='answer' id='c' required><span>\t${questions[questionNumber].answer.c}</span></label> 
+		 <label><input type='radio' name='answer' id='d' required><span>\t${questions[questionNumber].answer.d}</span></label> 
+		 </fieldset>
 		 <div id='submitArea'><button type='submit'>Submit</button></div></form>
 		`);
+	$('label').css({
+		'display': 'block',
+		'font-size': '2em'
+	});
+	$('#score').css('text-align', 'center');
+	$('fieldset').css({
+		'margin': '10px auto',
+		'width': '90%',
+		'text-align': 'left'
+	});
+	$('input[type=radio]').css('transform', 'scale(1.5)');
 };
 
 function handleStartButton(){
@@ -53,7 +64,7 @@ function correctAnswer(){
 
 function incorrectAnswer(){
 	let displayAnswer = questions[questionNumber].correctAnswer;
-	$('#submitArea').empty().append(`<h2>"Sorry! The correct answer was '${questions[questionNumber].answer[displayAnswer]}'!"</h2><div><button id='next'>NEXT</button></div>`);
+	$('#submitArea').empty().append(`<h2>"The correct answer was '${questions[questionNumber].answer[displayAnswer]}'!"</h2><div><button id='next'>NEXT</button></div>`);
 	$('#next').click(function(event){
 		event.preventDefault();
 		questionNumber++;
