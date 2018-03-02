6e078ee6f6ac989c0975214e986f529cce789daf
youngchul.kim <youngck.uw@gmail.com>
2018-03-02 08:29:23 -0800

    improving readability

--- index.js
+++ index.js
@@ -10,16 +10,21 @@ function displayQuiz(){
 		`<div id='questionText'><h2>${questions[questionNumber].question}</h2></div>
 		<form id='questionForm' action="/some-server-endpoint" method ='post'> 
 		<fieldset name='answerChoices'><legend><p>Answers:</p></legend>
-		<label><input type='radio' name='answer' id='a' required><span>\t${questions[questionNumber].answer.a}</span></label> 
-		<label><input type='radio' name='answer' id='b' required><span>\t${questions[questionNumber].answer.b}</span></label>
-		<label><input type='radio' name='answer' id='c' required><span>\t${questions[questionNumber].answer.c}</span></label> 
-		<label><input type='radio' name='answer' id='d' required><span>\t${questions[questionNumber].answer.d}</span></label> 
+		<label><input type='radio' name='answer' id='a' required>
+		<span>\t${questions[questionNumber].answer.a}</span></label> 
+		<label><input type='radio' name='answer' id='b' required>
+		<span>\t${questions[questionNumber].answer.b}</span></label>
+		<label><input type='radio' name='answer' id='c' required>
+		<span>\t${questions[questionNumber].answer.c}</span></label> 
+		<label><input type='radio' name='answer' id='d' required>
+		<span>\t${questions[questionNumber].answer.d}</span></label> 
 		</fieldset>
 		<div id='submitArea'><button type='submit'>Submit</button></div></form>`
 	);
 
 	$('footer').html(
-		`<div id='score'><p>Score ${score} | Questions ${questionNumber + "/" + questions.length}</p></div>`
+		`<div id='score'><p>Score ${score} 
+		| Questions ${questionNumber + "/" + questions.length}</p></div>`
 	);	
 };
 
@@ -30,7 +35,9 @@ function handleStartButton(){
 }
 
 function displayCongrats(){
-	$('main').html(`<p>Conguratuation!</p>\n<p>You have finished the quiz.</p>\n<p>Your score was ${score + "/" + questions.length}.</p><div><button id='try-again'>TRY AGAIN?</button></div>`);
+	$('main').html(`<p>Conguratuation!</p>\n
+	<p>Your score was ${score + "/" + questions.length}.
+	</p><div><button id='try-again'>TRY AGAIN?</button></div>`);
 	$('#try-again').on('click', function(){
 		score = 0;
 		questionNumber = 0;
@@ -47,7 +54,8 @@ function checkRemainingQuestions(questionNumber){
 }
 
 function correctAnswer(){
-	$('#submitArea').empty().append("<p>You are correct!</p><div><button id='next'>NEXT</button></div>");
+	$('#submitArea').empty().append(`<p>You are correct!</p>
+	<div><button id='next'>NEXT</button></div>`);
 	score++
 	$('#next').click(function(event){
 		event.preventDefault();
@@ -58,7 +66,9 @@ function correctAnswer(){
 
 function incorrectAnswer(){
 	let displayAnswer = questions[questionNumber].correctAnswer;
-	$('#submitArea').empty().append(`<p>"The correct answer was '${questions[questionNumber].answer[displayAnswer]}'!"</p><div><button id='next'>NEXT</button></div>`);
+	$('#submitArea').empty().append(`
+	<p>"The correct answer was '${questions[questionNumber].answer[displayAnswer]}'!"
+	</p><div><button id='next'>NEXT</button></div>`);
 	$('#next').click(function(event){
 		event.preventDefault();
 		questionNumber++;
